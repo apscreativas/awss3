6eced273bf5423444b2c9f746bb813bf095d2816
David Portales <ventas@apscreativas.com>
2017-11-15 17:41:00 -0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('s3-image-upload','S3ImageController@imageUpload');
+Route::post('s3-image-upload','S3ImageController@imageUploadPost');
+
+Route::get('upload_s3', function() {
+    $file = public_path() . '/uploads/david.jpg';
+    $upload = Storage::disk('s3')->put('david.jpg', file_get_contents($file), 'public');
+});
