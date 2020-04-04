25ca8cd9c713cd036887d09e277f6a45a448f33c
David Portales <ventas@apscreativas.com>
2020-04-04 12:08:25 -0600

    Update file
    Just delete a test rows

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,5 @@ Route::get('/', function () {
 Route::get('aws-s3','S3ImageController@imageUpload');
 Route::post('aws-s3','S3ImageController@imageUploadPost');
 
-Route::get('upload_s3', function() {
-    $file = public_path() . '/uploads/david.jpg';
-    $upload = Storage::disk('s3')->put('david.jpg', file_get_contents($file), 'public');
+
 });
